9735eb18d556e1c45b2551fe38f6a041757f729a
Michael Agbiaowei <101793657+michaelagbiaowei@users.noreply.github.com>
2023-03-28 15:17:29 +0100

    Create kompose.sh

---
+++ kompose.sh
@@ -0,0 +1,15 @@
+# USING BASH SCRIPT TO INSTALL KOMPOSE	
+
+#!/usr/bin/env bash
+
+# Step 1:
+# Download the instalation for Linux
+curl -L https://github.com/kubernetes/kompose/releases/download/v1.28.0/kompose-linux-amd64 -o kompose
+
+# Step 2:
+# Privileges
+chmod +x kompose
+
+# Step3:
+# Move binaries
+sudo mv ./kompose /usr/local/bin/kompose
