df85188cdaaffd3ec3fd38d480d7388798d8316f
Michael Agbiaowei <101793657+michaelagbiaowei@users.noreply.github.com>
2023-03-28 13:31:12 +0100

    Create hadolint.sh

---
+++ hadolint.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Step 1:
+# Installing Hadolint
+sudo wget -O /bin/hadolint https://github.com/hadolint/hadolint/releases/download/v2.10.0/hadolint-Linux-x86_64
+
+sudo chmod +x /bin/hadolint
