7e63a5f58764224d7044cd4ef19f53e79a549459
Michael Agbiaowei <101793657+michaelagbiaowei@users.noreply.github.com>
2023-04-30 13:17:19 +0100

    Update mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -3,8 +3,8 @@ apt-key list
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 sudo apt update
 wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_amd64.deb
-sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_sudo systemctl start mongod.serviceamd64.deb
 sudo apt install mongodb-org
+sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_sudo systemctl start mongod.serviceamd64.deb
 sudo systemctl start mongod.service
 sudo systemctl status mongod
 sudo systemctl status mongod
