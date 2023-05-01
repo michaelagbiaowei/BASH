68d0f8b8d0355c62506532e7c5f8d70b82363bf0
Michael Agbiaowei <101793657+michaelagbiaowei@users.noreply.github.com>
2023-05-01 15:10:40 +0100

    Update mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -1,10 +1,34 @@
+#!/usr/bin/env bash 
+
+# Step 1:
+# Import the public GPG key for the latest stable version of MongoDB
 curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+
+# Step 2#
+# To double check that the key was added correctly
 apt-key list
+
+# Step 3:
+# Create a file in the sources.list.d directory named mongodb-org-4.4.list
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+
+# Step 4:
+# Update the server’s local package index
 sudo apt update
+
+# Step 5:
+# Install libssl1.1 dependency
 wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb 
 sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2_amd64.deb
-sudo apt install mongodb-org
+
+# Step 6:
+# Install mongodb
+sudo apt install mongodb-org -y
+
+# Step 7:
+# Start mongod service
 sudo systemctl start mongod.service
-sudo systemctl status mongod
+
+# Step 8:
+# Confirm he  running status of mongod serivce
 sudo systemctl status mongod
