17a60fdaae470a3633b5d5d8691dbfec74caf50a
Michael Agbiaowei <101793657+michaelagbiaowei@users.noreply.github.com>
2023-05-29 10:44:02 +0100

    Update and rename cloudwatchagent to cloudwatchagent.sh

---
+++ cloudwatchagent.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Step 1:
+# Install CloudWatch agent package
+sudo yum install amazon-cloudwatch-agent
+
+# Step 2:
+# Install the CloudWatch agent
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/arm64/latest/amazon-cloudwatch-agent.rpm
+
+# Step 3:
+# Change to the directory containing the package
+sudo rpm -U ./amazon-cloudwatch-agent.rpm
+
+# Step 4:
+# Start the CloudWatch agent configuration wizard
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-config-wizard
+
+# Step 5:
+# Start the Clo
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:configuration-file-path # Edit the configuration-file-path to where itis stored
