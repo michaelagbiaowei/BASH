066754ca711e07cb046ab21a8303e108e4df995d
Michael Agbiaowei <101793657+michaelagbiaowei@users.noreply.github.com>
2023-09-16 09:24:19 +0100

    Create kind.sh

---
+++ kind.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Define the name of your Kind cluster (change as needed)
+CLUSTER_NAME="my-kind-cluster"
+
+# Check if Kind is installed, and if not, install it
+if ! command -v kind &> /dev/null; then
+  echo "Kind is not installed. Installing Kind..."
+  curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
+  chmod +x ./kind
+  sudo mv ./kind /usr/local/bin/kind
+fi
+
+# Check if the cluster with the given name already exists
+if kind get clusters | grep -q "$CLUSTER_NAME"; then
+  echo "Cluster '$CLUSTER_NAME' already exists. Deleting it..."
+  kind delete cluster --name "$CLUSTER_NAME"
+fi
+
+# Create a new Kind cluster
+echo "Creating Kind cluster '$CLUSTER_NAME'..."
+kind create cluster --name "$CLUSTER_NAME"
+
+# Set the kubeconfig context to use the new cluster
+kubectl config use-context "kind-$CLUSTER_NAME"
+
+# Verify cluster status
+kubectl cluster-info
+
+echo "Kind cluster '$CLUSTER_NAME' is deployed and ready for use."
